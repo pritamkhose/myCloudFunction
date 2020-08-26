2bb881ed9488a505f161658ce7fc96255521295d
Pritam <pritamkhose@gmail.com>
2020-08-26 15:21:19 +0530

    update version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # FirebaseCloudFunction
  Hello world from Firebase Cloud Function with simple express CORS API
+
+# Start & Install Project
+npm install -g firebase-tools
+
+# Login firebase account:
+firebase login
+
+# Initiate your project:
+firebase init
+
+# Localhost running:
+firebase serve
+
+# add exteral lib:
+cd functions
+npm i express cors
+cd ..
+
+# Deploy your functions:
+firebase deploy or npm run deploy
--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,15 @@ const functions = require('firebase-functions');
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-// http://localhost:5000
-// http://localhost:5000/node-pritam/us-central1/helloWorld
-// exports.helloWorld = functions.https.onRequest((request, response) => {
 const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase! @ " + new Date());
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
 });
-
+// http://localhost:5000/node-pritam/us-central1/helloWorld
 
 // https://itnext.io/running-express-js-on-firebase-cloud-functions-a20b536c6aec
 // https://codeburst.io/express-js-on-cloud-functions-for-firebase-86ed26f9144c
 
-// http://localhost:5000/node-pritam/us-central1/api2/express
 
 const cors = require("cors")
 const express = require("express")
@@ -22,20 +19,20 @@ const express = require("express")
 /* Express with CORS */
 const app2 = express()
 app2.use(cors({ origin: true }))
+
+
 app2.get("/express", (request, response) => {
     response.send("Hello from Express on Firebase with CORS!  @ " + new Date())
 })
 const api2 = functions.https.onRequest(app2)
+// http://localhost:5000/node-pritam/us-central1/api2/express
 
 module.exports = {
     helloWorld,
     api2
 }
 
-
-// localhost running - firebase serve
-// firebase deploy - npm run deploy
-
+// Project Workspace
 // https://console.firebase.google.com/u/2/project/node-pritam/functions
 
 // https://us-central1-node-pritam.cloudfunctions.net/helloWorld
